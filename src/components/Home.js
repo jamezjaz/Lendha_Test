2e2803ea8cb1db34349c494c9c5fdd5bc07b8fd2
Jamezjaz <jamezjaz@gmail.com>
2021-07-13 14:50:33 +0100

    docs: Style homepage buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
+import HomeStyles from '../styles/HomeStyles.module.css';
 
 const Home = () => (
   <>
@@ -10,11 +11,11 @@ const Home = () => (
     <h1>Welcome...</h1>
     <h2>This is Lendha Tech!</h2>
     <p>Go ahead and sign up if have not done so</p>
-    <button type="button">
-      <Link to="/register">Click here to sign up</Link>
+    <button type="button" className={HomeStyles.btn}>
+      <Link to="/register" style={{textDecoration: 'none'}}>Click here to sign up</Link>
     </button>
-    <button type="button">
-      <Link to="/request">Click here to fetch all data</Link>
+    <button type="button" className={HomeStyles.btn}>
+      <Link to="/request" style={{textDecoration: 'none'}}>Click here to fetch all data</Link>
     </button>
   </>
 );
